3e8196ac771ff6d1433c80558a3a8566402e2620
SariHop <sarihop12@gmail.com>
2024-11-07 09:28:05 +0200

    NavBar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface NavItem {
+    href: string;
+    text: string;
+}
+
+const navItems: NavItem[] = [
+    { href: '/pages/login', text: 'Login' },
+];
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className='flex justify-between p-5 bg-blue-700 text-white text-xl'>
+            <Link href='/'>My Website</Link>
+            <div className="flex space-x-4">
+            {navItems.map((item, index) => (
+                <Link key={index} href={item.href}>{item.text}</Link>
+            ))}
+        </div>
+      </nav >
+    );
+  };
+
+export default NavBar;
